278a3b3327fe98adb72160062eff4b8b4cec869a
itgsod-bahram-alzuhairi <bahram.alzuhairi@elev.ga.it-gymnasiet.se>
2017-10-18 14:06:56 +0200

    Printed age and category

--- sl.se.py
+++ sl.se.py
@@ -1,11 +1,19 @@
 def fares(age, student=False, senior=False):
-    print age,student,senior
-    return 'hel'
+    if student or senior:
+        return 'halv'
+    else:
+        if age < 20 or age> 65:
+            return 'halv'
+        else:
+            return 'hel'
 
 
 
+print 'barn 10',fares(10)
+print 'pensionar 65',fares(65)
+print 'sjukpensionar 47',fares(47,senior=True)
+print 'student 20',fares(25,student=True)
+print 'vuxen 45',fares(45)
 
 
-fares(10)
-fares(65,senior=True)
-fares(25,student=True)
+
